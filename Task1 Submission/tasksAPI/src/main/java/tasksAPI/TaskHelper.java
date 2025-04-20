316e098f0a79a25bee087e00b01633b98464dce5
Omar Alaa <152917715+omaralaa0@users.noreply.github.com>
2025-04-20 22:49:31 +0200

    Add files via upload

--- Task1 Submission/tasksAPI/src/main/java/tasksAPI/TaskHelper.java
+++ Task1 Submission/tasksAPI/src/main/java/tasksAPI/TaskHelper.java
@@ -1,7 +1,14 @@
 package tasksAPI;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.kie.api.task.model.TaskSummary;
+import org.kie.server.api.model.definition.TaskQueryFilterSpec;
+import org.kie.server.api.model.instance.TaskInstance;
 import org.kie.server.client.KieServicesClient;
+import org.kie.server.client.QueryServicesClient;
 import org.kie.server.client.UserTaskServicesClient;
 
 
@@ -9,18 +16,27 @@ public class TaskHelper {
 
 	public static void main(String[] args) {
 		TaskHelper task = new TaskHelper();
-//		task.startTask();
-		task.completeTask();
+		task.startTask();
+//		task.completeTask();
 		
 	}
+	
+
+
+	
 
 	
 	public void startTask() {
 		body util = new body();
 		KieServicesClient kieServicesClient = util.getKieServicesClient();
 		UserTaskServicesClient userClient = kieServicesClient.getServicesClient(UserTaskServicesClient.class);
-		userClient.startTask("test_1.0.0-SNAPSHOT", 4L, "wbadmin");
-		System.out.println("Task Started Succesfully");
+		QueryServicesClient queryClient = kieServicesClient.getServicesClient(QueryServicesClient.class);
+		TaskQueryFilterSpec filterSpec = new TaskQueryFilterSpec();
+		List<TaskInstance> taskList = queryClient.findHumanTasksWithFilters("jbpmHumanTasksWithUser", filterSpec, 0, 10);
+		for (TaskInstance task : taskList) {
+		    System.out.println("Task ID: " + task.getId() + ", Name: " + task.getName() + ", Status: " + task.getStatus());
+		}
+
 	}
 	
 	public void completeTask() {
